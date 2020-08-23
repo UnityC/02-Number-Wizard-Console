78ae8126cdddee1e7175d8270755d53841102cf0
Noor Mahammad Chervu <noormahammad@users.noreply.github.com>
2020-08-22 21:03:51 -0400

    restart game once number guessed

--- New Unity Project/Assets/HelloWorld.cs
+++ New Unity Project/Assets/HelloWorld.cs
@@ -10,41 +10,55 @@ public class HelloWorld : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        startGame();
+    {        
+        StartGame();
     }
 
-    void startGame()
+    void ReStart()
+    {
+        max = 1000;
+        min = 1;
+        StartGame();
+    }
+
+    void StartGame()
     {
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number, don't tell me yet");
         Debug.Log($"Highest number you can pick is <b>{max}</b>");
         Debug.Log($"Lowest number is <b>{min}</b>");
-        Guess(min, max);
+        NextGuess(min, max);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            ReStart();
+        }
+
         if (Input.GetKeyUp(KeyCode.UpArrow)) {         
             min = guess;
-            Guess(min, max);
+            NextGuess(min, max);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) {         
             max = guess;
-            Guess(min, max);
+            NextGuess(min, max);
         }
         else if (Input.GetKeyUp(KeyCode.Return)) {         
             Debug.Log($"Your number is <b><size=20><color=green>{guess}</color></size></b>");
-        }
+            ReStart();
+        }       
     }
 
-    void Guess(int min, int max)
+    void NextGuess(int min, int max)
     {
         guess = (min + max) / 2;
 
-        if (max-min == 2) {
+        if (max-min <= 2) {
             Debug.Log($"Your number is <b><color=green>{guess}</color></b>");
+            ReStart();
             return;
         }
         
